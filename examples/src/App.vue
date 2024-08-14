bc47216a34183ad698a490a8179cbcafd113dea1
saqqdy <saqqdy@qq.com>
2024-08-14 12:09:03 +0800

    release 1.0.0

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,37 +1,92 @@
 <script setup>
 import { ref } from 'vue'
+import until from 'untiljs'
+
+const val1 = ref(1)
+const val1Changed = ref(false)
+const testToBe = async () => {
+	setTimeout(() => {
+		val1.value = 2
+	}, 1000)
+
+	await until(val1).toBe(2)
+	val1Changed.value = true
+}
+
+const val2 = ref(1)
+const val2Changed = ref(false)
+const testCustomConditions = async () => {
+	setTimeout(() => {
+		val2.value = 2
+	}, 1000)
+
+  	await until(val2).toMatch(value => value > 1)
+	val2Changed.value = true
+}
+
+const val3 = ref(1)
+const val3Changed = ref(false)
+const testTimeout = async () => {
+	setTimeout(() => {
+		val3.value = 2
+	}, 1000)
+
+  	await until(val3).not.toBe(ref(2), { timeout: 500, throwOnTimeout: true })
+	val3Changed.value = true
+}
+
+const val4 = ref(1)
+const val4Changed = ref(false)
+const testOthers = async () => {
+	setTimeout(() => {
+		val4.value = 2
+	}, 1000)
+
+	await until(val4).toBe(true)
+	await until(val4).toMatch(v => v > 5 && v < 10)
+	await until(val4).changed()
+	await until(val4).changedTimes(2)
+	await until(val4).toBeTruthy()
+	await until(val4).toBeNull()
+
+	await until(val4).not.toBeNull()
+	await until(val4).not.toBeTruthy()
+	val4Changed.value = true
+}
 
 defineExpose({
-	cancelledRequestData,
-	request1,
-	request2,
-	request3,
-	request4,
-	request5,
-	lastRequestID,
-	doRequestUnique,
-	doRequestOrderly
+	val1,
+	val1Changed,
+	testToBe,
+	val2,
+	val2Changed,
+	testCustomConditions,
+	val3,
+	val3Changed,
+	testTimeout,
+	val4,
+	val4Changed,
+	testOthers,
 })
 </script>
 
 <template>
 	<div>
-		<button type="button" @click="doRequestUnique">Do Request (cancel similar request)</button>
+		<div>val1: {{ val1 }}</div>
+		<div>val1Changed: {{ val1Changed }}</div>
+		<button type="button" @click="testToBe">Test .toBe</button>
 		<p></p>
-		<div>request 1: {{ request1 }}</div>
+		<div>val2: {{ val2 }}</div>
+		<div>val2Changed: {{ val2Changed }}</div>
+		<button type="button" @click="testCustomConditions">Test custom conditions</button>
 		<p></p>
-		<div>request 2: {{ request2 }}</div>
+		<div>val3: {{ val3 }}</div>
+		<div>val3Changed: {{ val3Changed }}</div>
+		<button type="button" @click="testTimeout">Test timeout</button>
 		<p></p>
-		<div>cancelled request data: {{ cancelledRequestData }}</div>
+		<div>val4: {{ val4 }}</div>
+		<div>val4Changed: {{ val4Changed }}</div>
+		<button type="button" @click="testOthers">Test others</button>
 		<p></p>
-		<button type="button" @click="doRequestOrderly">Do Request (return in sequence)</button>
-		<p></p>
-		<div>request 3: {{ request3 }}</div>
-		<p></p>
-		<div>request 4: {{ request4 }}</div>
-		<p></p>
-		<div>request 5: {{ request5 }}</div>
-		<p></p>
-		<div>last request id is: {{ lastRequestID }}</div>
 	</div>
 </template>
