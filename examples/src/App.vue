637675b9a594309ad4c00cb93e4600e900d6918a
saqqdy <saqqdy@qq.com>
2024-08-14 12:35:03 +0800

    release 1.0.0

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -25,33 +25,55 @@ const testCustomConditions = async () => {
 }
 
 const val3 = ref(1)
-const val3Changed = ref(false)
+const val3Value = ref(1)
 const testTimeout = async () => {
 	setTimeout(() => {
 		val3.value = 2
 	}, 1000)
 
-  	await until(val3).not.toBe(ref(2), { timeout: 500, throwOnTimeout: true })
-	val3Changed.value = true
+  	await until(val3).toBe(ref(2), { timeout: 500, throwOnTimeout: true })
+	val3Value.value = val3.value
 }
 
 const val4 = ref(1)
-const val4Changed = ref(false)
+const val4_toBeTrue = ref(false)
+const val4_toMatch5_10 = ref(false)
+const val4_changed = ref(false)
+const val4_changedTimes2 = ref(false)
+const val4_toBeTruthy = ref(false)
+const val4_toBeNull= ref(false)
+const val4_not_toBeNull = ref(false)
+const val4_not_toBeTruthy = ref(false)
 const testOthers = async () => {
 	setTimeout(() => {
 		val4.value = 2
 	}, 1000)
 
-	await until(val4).toBe(true)
-	await until(val4).toMatch(v => v > 5 && v < 10)
-	await until(val4).changed()
-	await until(val4).changedTimes(2)
-	await until(val4).toBeTruthy()
-	await until(val4).toBeNull()
+	until(val4).toBe(true, { timeout: 1500 }).then(() => {
+		toBeTrue.value = true
+	})
+	 until(val4).toMatch(v => v > 5 && v < 10, { timeout: 1500 }).then(() => {
+		val4_toMatch5_10.value = true
+	})
+	 until(val4).changed({ timeout: 1500 }).then(() => {
+		val4_changed.value = true
+	})
+	 until(val4).changedTimes(2, { timeout: 1500 }).then(() => {
+		val4_changedTimes2.value = true
+	})
+	 until(val4).toBeTruthy({ timeout: 1500 }).then(() => {
+		val4_toBeTruthy.value = true
+	})
+	 until(val4).toBeNull( { timeout: 1500 }).then(() => {
+		val4_toBeNull.value = true
+	})
 
-	await until(val4).not.toBeNull()
-	await until(val4).not.toBeTruthy()
-	val4Changed.value = true
+	 until(val4).not.toBeNull({ timeout: 1500 }).then(() => {
+		val4_not_toBeNull.value = true
+	})
+	 until(val4).not.toBeTruthy({ timeout: 1500 }).then(() => {
+		val4_not_toBeTruthy.value = true
+	})
 }
 
 defineExpose({
@@ -65,7 +87,14 @@ defineExpose({
 	val3Changed,
 	testTimeout,
 	val4,
-	val4Changed,
+	val4_toBeTrue,
+	val4_toMatch5_10,
+	val4_changed,
+	val4_changedTimes2,
+	val4_toBeTruthy,
+	val4_toBeNull,
+	val4_not_toBeNull,
+	val4_not_toBeTruthy,
 	testOthers,
 })
 </script>
@@ -81,11 +110,18 @@ defineExpose({
 		<button type="button" @click="testCustomConditions">Test custom conditions</button>
 		<p></p>
 		<div>val3: {{ val3 }}</div>
-		<div>val3Changed: {{ val3Changed }}</div>
+		<div>val3Value: {{ val3Value }}</div>
 		<button type="button" @click="testTimeout">Test timeout</button>
 		<p></p>
 		<div>val4: {{ val4 }}</div>
-		<div>val4Changed: {{ val4Changed }}</div>
+		<div>val4_toBeTrue: {{ val4_toBeTrue }}</div>
+		<div>val4_toMatch5_10: {{ val4_toMatch5_10 }}</div>
+		<div>val4_changed: {{ val4_changed }}</div>
+		<div>val4_changedTimes2: {{ val4_changedTimes2 }}</div>
+		<div>val4_toBeTruthy: {{ val4_toBeTruthy }}</div>
+		<div>val4_toBeNull: {{ val4_toBeNull }}</div>
+		<div>val4_not_toBeNull: {{ val4_not_toBeNull }}</div>
+		<div>val4_not_toBeTruthy: {{ val4_not_toBeTruthy }}</div>
 		<button type="button" @click="testOthers">Test others</button>
 		<p></p>
 	</div>
